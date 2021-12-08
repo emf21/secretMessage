7cbb45065abc5535c038679629a726e448e96a89
emf21 <emefa.ameku@amalitech.org>
2021-12-08 17:40:50 +0000

    Secret Message

---
+++ secretMessage.js
@@ -0,0 +1,47 @@
+// Project (Secret Message)
+// This project uses ways of accessing arrays with its methods to print a secret message
+
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push("to", "Program");
+secretMessage[7] = "right";
+secretMessage.shift();
+secretMessage.unshift("Programming");
+console.log(secretMessage);
+
+// this splice means delete from the 6th index to the 5th item, then replace with 'know'
+secretMessage.splice(6, 5, "know,");
+console.log(secretMessage);
+
+// .join puts array in a single sentence
+console.log(secretMessage.join(" "));
+
+// prints
+// Programming is not about what you know,
+// it is about what you can figure out. -2015, Chris Pine, Learn to Program
